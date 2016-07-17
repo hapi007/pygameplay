9560d6feea4203aaecc078ef9d991fe6b20940cb
Rene Dudfield <renesd@gmail.com>
2016-07-17 14:56:34 +0200

    travising

---
+++ .travis_osx_before_install.sh
@@ -0,0 +1,42 @@
+# Compiles pygame on homebrew for distribution.
+# This may not be what you want to do if not on travisci.
+
+brew update
+brew unlink pkg-config
+brew install pkg-config
+brew unlink libpng
+brew unlink libtiff
+
+
+brew uninstall --force sdl
+brew uninstall --force sdl_image
+brew uninstall --force sdl_mixer
+brew uninstall --force sdl_ttf
+brew uninstall --force smpeg
+brew uninstall --force jpeg
+brew uninstall --force libpng
+brew uninstall --force libtiff
+brew uninstall --force webp
+brew uninstall --force flac
+brew uninstall --force fluid-synth
+brew uninstall --force smpeg
+brew uninstall --force libmikmod
+brew uninstall --force libvorbis
+brew uninstall --force portmidi
+brew uninstall --force freetype
+brew install jpeg --universal
+brew install libpng --universal
+brew install libtiff --universal --with-xz
+brew install webp --universal --with-libtiff --with-giflib
+brew install libvorbis --universal
+brew install libogg --universal
+brew install flac --universal --with-libogg
+brew install fluid-synth
+brew install libmikmod --universal
+brew install smpeg
+brew install portmidi
+brew install freetype --universal
+brew install sdl --without-x --universal
+brew install sdl_ttf --universal
+brew install sdl_image --universal
+brew install sdl_mixer --universal --with-flac --with-fluid-synth --with-libmikmod --with-libvorbis --with-smpeg
---
+++ .travis_osx_install.sh
@@ -0,0 +1,21 @@
+
+# ls -la /usr/local/lib/
+# ls -la /usr/local/include/
+# cat Setup
+# otool -L `find . -name imageext.so`
+# otool -L `find . -name image.so`
+# otool -L /usr/local/opt/sdl_image/lib/libSDL_image-1.2.0.dylib
+
+
+# Removes any X11R6 stuff so that png headers are not picked up by accident.
+sed 's/-I\/usr\/X11R6\/include //g' Setup > Setup.new
+mv Setup.new Setup
+
+# Makes for i386 and x86_64.
+CXXFLAGS="-arch i386 -arch x86_64" CFLAGS="-arch i386 -arch x86_64" LDFLAGS="-arch i386 -arch x86_64" $PYTHON_EXE setup.py install -noheaders
+CXXFLAGS="-arch i386 -arch x86_64" CFLAGS="-arch i386 -arch x86_64" LDFLAGS="-arch i386 -arch x86_64" python setup.py bdist_wheel
+
+# This copies in the .dylib files that are linked to the .so files.
+# It also rewrites the .so file linking options. https://pypi.python.org/pypi/delocate
+$PIP_CMD install delocate
+delocate-wheel -v dist/*.whl
